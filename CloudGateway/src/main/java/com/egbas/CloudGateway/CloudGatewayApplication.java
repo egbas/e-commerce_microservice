df78ceba39239cf89d7b5e4493528ea8d6af2162
Onaivi Emmanuel <113881132+egbas@users.noreply.github.com>
2024-11-06 12:39:12 +0100

    minor

--- CloudGateway/src/main/java/com/egbas/CloudGateway/CloudGatewayApplication.java
+++ CloudGateway/src/main/java/com/egbas/CloudGateway/CloudGatewayApplication.java
@@ -1,8 +1,13 @@
 package com.egbas.CloudGateway;
 
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -12,4 +17,13 @@ public class CloudGatewayApplication {
 		SpringApplication.run(CloudGatewayApplication.class, args);
 	}
 
+	@Bean
+	public Customizer<Resilience4JCircuitBreakerFactory> defaultCusomizer(){
+		return factory -> factory.configureDefault(
+				id -> new Resilience4JConfigBuilder(id)
+						.circuitBreakerConfig(
+								CircuitBreakerConfig.ofDefaults()
+						).build()
+		);
+	}
 }
